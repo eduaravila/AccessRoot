7b04f4e5e1b01c1cfa2a0b48b2d11a891d04fd8d
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2020-02-28 13:12:06 -0600

    heroku deploy

--- dist/src/index.js
+++ dist/src/index.js
@@ -16,46 +16,42 @@ require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const body_parser_1 = __importDefault(require("body-parser"));
-const serverless_http_1 = __importDefault(require("serverless-http"));
 const buildFederatedSchema_1 = require("./helpers/buildFederatedSchema");
 //?  decorators metadata
 const index_1 = __importDefault(require("./DB/index"));
 const UserResolver_1 = require("./resolvers/UserResolver");
-const PORT = process.env.PORT || "3000";
-// Initialize the app
-const app = express_1.default();
-app.use(/\/((?!graphql).)*/, body_parser_1.default.urlencoded({
-    limit: "50mb",
-    extended: true
-}));
-app.use(/\/((?!graphql).)*/, body_parser_1.default.json({
-    limit: "50mb"
-}));
-const router = express_1.default.Router();
-router.get("/", (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Hello from Express.js!</h1>");
-    res.end();
-});
-// app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
-    res.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,UPDATE,PUT"); //* metodos permitidos por el cliente
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    //* dominios por donde se permite el acceso
-    //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
-    // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
-    if (req.method === "OPTIONS") {
-        res.sendStatus(200);
-    }
-    next();
-});
-const handler = serverless_http_1.default(app);
-module.exports = app;
-module.exports.handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
+(() => __awaiter(void 0, void 0, void 0, function* () {
     // you can do other things here
-    const result = yield handler(event, context);
     try {
+        const PORT = process.env.PORT || "3000";
+        // Initialize the app
+        const app = express_1.default();
+        app.use(/\/((?!graphql).)*/, body_parser_1.default.urlencoded({
+            limit: "50mb",
+            extended: true
+        }));
+        app.use(/\/((?!graphql).)*/, body_parser_1.default.json({
+            limit: "50mb"
+        }));
+        const router = express_1.default.Router();
+        router.get("/", (req, res) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write("<h1>Hello from Express.js!</h1>");
+            res.end();
+        });
+        // app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
+        app.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
+            res.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,UPDATE,PUT"); //* metodos permitidos por el cliente
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            //* dominios por donde se permite el acceso
+            //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
+            // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
+            if (req.method === "OPTIONS") {
+                res.sendStatus(200);
+            }
+            next();
+        });
         const server = new apollo_server_express_1.ApolloServer({
             schema: yield buildFederatedSchema_1.buildFederatedSchema({
                 resolvers: [UserResolver_1.RegisterResolver]
@@ -73,13 +69,12 @@ module.exports.handler = (event, context) => __awaiter(void 0, void 0, void 0, f
         // Start the server
         yield index_1.default();
         server.applyMiddleware({ app, path: "/graphql" });
-        // app.listen(PORT, () => {
-        //   console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
-        // });
+        app.listen(PORT, () => {
+            console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
+        });
     }
     catch (error) {
         console.log(error);
     }
-    return result;
-});
+}))();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -11,52 +11,54 @@ import { buildFederatedSchema } from "./helpers/buildFederatedSchema";
 import connectDB from "./DB/index";
 import { RegisterResolver } from "./resolvers/UserResolver";
 
-const PORT: string = process.env.PORT || "3000";
-// Initialize the app
-const app = express();
-app.use(
-  /\/((?!graphql).)*/,
-  bp.urlencoded({
-    limit: "50mb",
-    extended: true
-  })
-);
-app.use(
-  /\/((?!graphql).)*/,
-  bp.json({
-    limit: "50mb"
-  })
-);
-
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
-  res.end();
-});
-
-// app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,UPDATE,PUT"); //* metodos permitidos por el cliente
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  //* dominios por donde se permite el acceso
-  //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
-  // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  }
-  next();
-});
-
-const handler = serverless(app);
-
-module.exports = app;
-module.exports.handler = async (event: any, context: any) => {
+(async () => {
   // you can do other things here
-  const result = await handler(event, context);
   try {
+    const PORT: string = process.env.PORT || "3000";
+    // Initialize the app
+    const app = express();
+    app.use(
+      /\/((?!graphql).)*/,
+      bp.urlencoded({
+        limit: "50mb",
+        extended: true
+      })
+    );
+    app.use(
+      /\/((?!graphql).)*/,
+      bp.json({
+        limit: "50mb"
+      })
+    );
+
+    const router = express.Router();
+    router.get("/", (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Hello from Express.js!</h1>");
+      res.end();
+    });
+
+    // app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
+
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST,GET,DELETE,UPDATE,PUT"
+      ); //* metodos permitidos por el cliente
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type,Authorization"
+      );
+      //* dominios por donde se permite el acceso
+      //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
+      // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
+      if (req.method === "OPTIONS") {
+        res.sendStatus(200);
+      }
+      next();
+    });
+
     const server = new ApolloServer({
       schema: await buildFederatedSchema({
         resolvers: [RegisterResolver]
@@ -76,11 +78,10 @@ module.exports.handler = async (event: any, context: any) => {
     await connectDB();
     server.applyMiddleware({ app, path: "/graphql" });
 
-    // app.listen(PORT, () => {
-    //   console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
-    // });
+    app.listen(PORT, () => {
+      console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
+    });
   } catch (error) {
     console.log(error);
   }
-  return result;
-};
+})();
